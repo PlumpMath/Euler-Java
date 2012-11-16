b3c9b2cf8b362045294672c077dbeedc59be4207
nicklink483 <nicklink483@gmail.com>
2012-11-16 05:01:58 -0800

    ''

--- src/ProblemTwo.java
+++ src/ProblemTwo.java
@@ -1,20 +1,18 @@
 public class ProblemTwo {
   public static void main(String[] args){
     //find the sum of the even-numbered Fibonacci numbers not exceeding 4 million
-    long prev = 1;
-    long curr = 1;
-    long next = 2;
-    long memo = 0;
-    while (curr < 4000000) {
+    long currentNumber = 1;
+    long nextNumber = 2;
+    long sum = 0;
+    while (currentNumber < 4000000) {
     
-      if(curr%2==0){
-        memo += curr;
+      if(currentNumber%2==0){
+        sum += currentNumber;
       }
       
-      next += curr;
-      curr = next - curr;
-      prev = next - curr;
+      nextNumber += currentNumber;
+      currentNumber = nextNumber - currentNumber;
     }
-    System.out.println(memo);
+    System.out.println(sum);
   }
 }
