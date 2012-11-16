6ef5ad92e14c985ea01b4ea4ab1d30a10a765896
nicklink483 <nicklink483@gmail.com>
2012-11-16 09:22:26 -0800

    ''

---
+++ src/ProblemSix.java
@@ -0,0 +1,15 @@
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+public class ProblemSix {
+  public static void main (String[] args) {
+    int sumOfSquares = 0;
+    int squareOfSum = 0;
+    for( int i = 1; i <= 100; i++ ) {
+      sumOfSquares += i*i;
+      squareOfSum += i;
+    }
+    squareOfSum *= squareOfSum;
+    int answer = Math.abs(sumOfSquares-squareOfSum);
+    System.out.println("The answer is: " + answer);
+  }
+}
