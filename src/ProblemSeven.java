a745332caefbb736cca3482a4f0c123fef313e1b
nicklink483 <nicklink483@gmail.com>
2012-11-16 17:55:46 -0800

    ''

---
+++ src/ProblemSeven.java
@@ -0,0 +1,24 @@
+//What is the 10 001st prime number?
+
+public class ProblemSeven {
+  public static void main (String[] args) {
+    int counter = 0;
+    int currentNumber = 1;
+    while (counter < 10001) {
+      currentNumber++;
+      if (isPrime(currentNumber)) {
+        counter++;
+      }
+    }
+    System.out.println(currentNumber);
+  }
+  private static boolean isPrime(long n){
+  //really inefficient way of checking if a number is prime
+    for (long i=2;i<n;i++){
+      if (n%i==0){
+        return false;
+      }
+    }
+    return true;
+  }
+}
